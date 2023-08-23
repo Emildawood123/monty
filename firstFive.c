9f329af862521d44e170e8bbf72e36eb3bd14461
emil dawood <edawood997@gmail.com>
2023-08-23 02:40:56 -0700

    stderr instead of stdout

--- firstFive.c
+++ firstFive.c
@@ -86,7 +86,7 @@ void pint(stack_t **stack, __attribute__((unused)) unsigned int line)
 if (*stack == NULL || stack == NULL)
 {
 free_stack(stack);
-fprintf(stdout, "L%d: can't pint, stack empty\n", line);
+fprintf(stderr, "L%d: can't pint, stack empty\n", line);
 exit(EXIT_FAILURE);
 }
 fprintf(stdout, "%d\n", (*stack)->n);
