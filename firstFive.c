30093de1c4f4bfbc0c47039e1ef90cf50401f005
emil dawood <edawood997@gmail.com>
2023-08-23 01:57:21 -0700

    good free

--- firstFive.c
+++ firstFive.c
@@ -83,6 +83,7 @@ void pint(stack_t **stack, __attribute__((unused)) unsigned int line)
 {
 if (*stack == NULL || stack == NULL)
 {
+free_stack(stack);
 fprintf(stdout, "L%d: can't pint, stack empty", line);
 exit(EXIT_FAILURE);
 }
