dcb010974f3bb066e27dab97ff82eb5bd1084a76
emil dawood <edawood997@gmail.com>
2023-08-23 03:05:51 -0700

    remove unused in monty.h

--- monty.h
+++ monty.h
@@ -43,8 +43,8 @@ int is_valid(char *line, instruction_t opcode[]);
 char *new_line(char *line);
 void er_ins(int count, char *new_line, char *line);
 void er_argc(void);
-void pint(stack_t **stack, __attribute__((unused)) unsigned int n);
+void pint(stack_t **stack, unsigned int n);
 int is_d(char *value);
 void pop(stack_t **stack, unsigned int line);
-void swap(stack_t **stack, __attribute__((unused)) unsigned int line);
+void swap(stack_t **stack, unsigned int line);
 #endif
