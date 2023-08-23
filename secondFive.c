46c3a3d8bc45e5596fa56500dd9099d12d57053f
emil dawood <edawood997@gmail.com>
2023-08-23 03:01:11 -0700

    good

--- secondFive.c
+++ secondFive.c
@@ -30,16 +30,14 @@ else
 void swap(stack_t **stack, __attribute__((unused)) unsigned int line)
 {
 int num;
-stack_t *new;
-new = *stack;
 if (*stack == NULL || (*stack)->next == NULL)
 {
 free_stack(&new);
 free_stack(stack);
-fprintf(stderr, "L%d: can't swap, stack too short", line);
+fprintf(stderr, "L%d: can't swap, stack too short\n", line);
 exit(EXIT_FAILURE);
 }
-num = new->n;
-new->n = new->next->n;
-new->next->n = num;
+num = (*stack)->n;
+(*stack)->n = (*stack)->next->n;
+(*stack)->next->n = num;
 }
