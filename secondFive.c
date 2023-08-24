6072bfdb8d596ec951975cfa8f0e16d0de891966
emil dawood <edawood997@gmail.com>
2023-08-24 04:46:43 -0700

    done

--- secondFive.c
+++ secondFive.c
@@ -76,13 +76,16 @@ __attribute__((unused)) unsigned int line)
  */
 void sub(stack_t **stack, __attribute__((unused)) unsigned int line)
 {
+stack_t *new;
 if (*stack == NULL || (*stack)->next == NULL)
 {
 free_stack(stack);
 fprintf(stderr, "L%d: can't sub, stack too short\n", line);
 exit(EXIT_FAILURE);
 }
+new = *stack;
 (*stack)->next->n = (*stack)->n - (*stack)->next->n;
 (*stack)->next->prev = NULL;
+free(new);
 (*stack) = (*stack)->next;
 }
