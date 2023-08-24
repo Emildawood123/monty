9608429813b9044782fb7333ebdd454c9d6df796
emil dawood <edawood997@gmail.com>
2023-08-24 03:51:18 -0700

    dddd

--- secondFive.c
+++ secondFive.c
@@ -32,7 +32,6 @@ void swap(stack_t **stack, __attribute__((unused)) unsigned int line)
 stack_t *new;
 if (*stack == NULL || (*stack)->next == NULL)
 {
-free_stack(stack);
 fprintf(stderr, "L%d: can't swap, stack too short\n", line);
 exit(EXIT_FAILURE);
 }
