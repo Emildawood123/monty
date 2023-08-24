c359ec4dde97a0caf9e13e2b6f9d49deb98a2dff
emil dawood <edawood997@gmail.com>
2023-08-24 04:54:23 -0700

    done

--- secondFive.c
+++ secondFive.c
@@ -56,8 +56,8 @@ exit(EXIT_FAILURE);
 new = *stack;
 (*stack)->next->n = (*stack)->n + (*stack)->next->n;
 (*stack)->next->prev = NULL;
-free(new);
 (*stack) = (*stack)->next;
+free(new);
 }
 /**
  * nop - nothing
@@ -86,6 +86,6 @@ exit(EXIT_FAILURE);
 new = *stack;
 (*stack)->next->n = (*stack)->next->n - (*stack)->n;
 (*stack)->next->prev = NULL;
-free(new);
 (*stack) = (*stack)->next;
+free(new);
 }
